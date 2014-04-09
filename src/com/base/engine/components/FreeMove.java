b06bc1da5c40ab16b71db3a5c49b2d9e408aca87
16nb31668prg <16nb31668prg@YHSD-A261-03.elmhurst205.org>
2014-04-09 15:10:17 -0500

    Edited freemove

--- src/com/base/engine/components/FreeMove.java
+++ src/com/base/engine/components/FreeMove.java
@@ -3,45 +3,48 @@ package com.base.engine.components;
 import com.base.engine.core.Input;
 import com.base.engine.core.Vector3f;
 
-public class FreeMove extends GameComponent
-{
+public class FreeMove extends GameComponent {
 	private float speed;
 	private int forwardKey;
 	private int backKey;
 	private int leftKey;
 	private int rightKey;
+	private int upKey;
+	private int downKey;
 
-	public FreeMove(float speed)
-	{
-		this(speed, Input.KEY_W, Input.KEY_S, Input.KEY_A, Input.KEY_D);
+	public FreeMove(float speed) {
+		this(speed, Input.KEY_W, Input.KEY_S, Input.KEY_A, Input.KEY_D, Input.KEY_SPACE, Input.KEY_LSHIFT);
 	}
 
-	public FreeMove(float speed, int forwardKey, int backKey, int leftKey, int rightKey)
-	{
+	public FreeMove(float speed, int forwardKey, int backKey, int leftKey, int rightKey, int upKey, int downKey) {
 		this.speed = speed;
 		this.forwardKey = forwardKey;
 		this.backKey = backKey;
 		this.leftKey = leftKey;
 		this.rightKey = rightKey;
+		this.upKey = upKey;
+		this.downKey = downKey;
 	}
 
 	@Override
-	public void input(float delta)
-	{
+	public void input(float delta) {
 		float movAmt = speed * delta;
 
-		if(Input.getKey(forwardKey))
+		if (Input.getKey(forwardKey))
 			move(getTransform().getRot().getForward(), movAmt);
-		if(Input.getKey(backKey))
+		if (Input.getKey(backKey))
 			move(getTransform().getRot().getForward(), -movAmt);
-		if(Input.getKey(leftKey))
+		if (Input.getKey(leftKey))
 			move(getTransform().getRot().getLeft(), movAmt);
-		if(Input.getKey(rightKey))
+		if (Input.getKey(rightKey))
 			move(getTransform().getRot().getRight(), movAmt);
+		if (Input.getKey(upKey))
+			move(getTransform().getRot().getUp(), movAmt / 2);
+		if (Input.getKey(downKey))
+			move(getTransform().getRot().getDown(), movAmt / 2);
 	}
 
-	private void move(Vector3f dir, float amt)
-	{
+	private void move(Vector3f dir, float amt) {
 		getTransform().setPos(getTransform().getPos().add(dir.mul(amt)));
 	}
 }
