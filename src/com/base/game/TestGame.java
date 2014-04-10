9400d568fae15bca674d0253808d0676239f6d70
Noah <noah.bergl@gmail.com>
2014-04-10 15:02:45 -0500

    Just some Changes

--- src/com/base/game/TestGame.java
+++ src/com/base/game/TestGame.java
@@ -4,28 +4,18 @@ import com.base.engine.components.*;
 import com.base.engine.core.*;
 import com.base.engine.rendering.*;
 
-public class TestGame extends Game
-{
-	public void init()
-	{
+public class TestGame extends Game {
+	public void init() {
 		float fieldDepth = 10.0f;
 		float fieldWidth = 10.0f;
 
-		Vertex[] vertices = new Vertex[] { 	new Vertex( new Vector3f(-fieldWidth, 0.0f, -fieldDepth), new Vector2f(0.0f, 0.0f)),
-				new Vertex( new Vector3f(-fieldWidth, 0.0f, fieldDepth * 3), new Vector2f(0.0f, 1.0f)),
-				new Vertex( new Vector3f(fieldWidth * 3, 0.0f, -fieldDepth), new Vector2f(1.0f, 0.0f)),
-				new Vertex( new Vector3f(fieldWidth * 3, 0.0f, fieldDepth * 3), new Vector2f(1.0f, 1.0f))};
+		Vertex[] vertices = new Vertex[] { new Vertex(new Vector3f(-fieldWidth, 0.0f, -fieldDepth), new Vector2f(0.0f, 0.0f)), new Vertex(new Vector3f(-fieldWidth, 0.0f, fieldDepth * 3), new Vector2f(0.0f, 1.0f)), new Vertex(new Vector3f(fieldWidth * 3, 0.0f, -fieldDepth), new Vector2f(1.0f, 0.0f)), new Vertex(new Vector3f(fieldWidth * 3, 0.0f, fieldDepth * 3), new Vector2f(1.0f, 1.0f)) };
 
-		int indices[] = { 0, 1, 2,
-				2, 1, 3};
+		int indices[] = { 0, 1, 2, 2, 1, 3 };
 
-		Vertex[] vertices2 = new Vertex[] { 	new Vertex( new Vector3f(-fieldWidth/ 10, 0.0f, -fieldDepth/ 10), new Vector2f(0.0f, 0.0f)),
-				new Vertex( new Vector3f(-fieldWidth/ 10, 0.0f, fieldDepth/ 10 * 3), new Vector2f(0.0f, 1.0f)),
-				new Vertex( new Vector3f(fieldWidth/ 10 * 3, 0.0f, -fieldDepth/ 10), new Vector2f(1.0f, 0.0f)),
-				new Vertex( new Vector3f(fieldWidth/ 10 * 3, 0.0f, fieldDepth/ 10 * 3), new Vector2f(1.0f, 1.0f))};
+		Vertex[] vertices2 = new Vertex[] { new Vertex(new Vector3f(-fieldWidth / 10, 0.0f, -fieldDepth / 10), new Vector2f(0.0f, 0.0f)), new Vertex(new Vector3f(-fieldWidth / 10, 0.0f, fieldDepth / 10 * 3), new Vector2f(0.0f, 1.0f)), new Vertex(new Vector3f(fieldWidth / 10 * 3, 0.0f, -fieldDepth / 10), new Vector2f(1.0f, 0.0f)), new Vertex(new Vector3f(fieldWidth / 10 * 3, 0.0f, fieldDepth / 10 * 3), new Vector2f(1.0f, 1.0f)) };
 
-		int indices2[] = { 0, 1, 2,
-				2, 1, 3};
+		int indices2[] = { 0, 1, 2, 2, 1, 3 };
 
 		Mesh mesh2 = new Mesh(vertices2, indices2, true);
 
@@ -36,11 +26,12 @@ public class TestGame extends Game
 		material.addFloat("specularIntensity", 1);
 		material.addFloat("specularPower", 8);
 
-		Material material2 = new Material();// new Texture("test.png"), new
-											// Vector3f(1,1,1), 1, 8);
-		material2.addTexture("diffuse", new Texture("test.png"));
-		material2.addFloat("specularIntensity", 1);
-		material2.addFloat("specularPower", 8);
+		/*
+		 * Material material2 = new Material();// new Texture("test.png"), new
+		 * // Vector3f(1,1,1), 1, 8); material2.addTexture("diffuse", new
+		 * Texture("test.png")); material2.addFloat("specularIntensity", 1);
+		 * material2.addFloat("specularPower", 8);
+		 */
 
 		Mesh tempMesh = new Mesh("monkey3.obj");
 
@@ -68,36 +59,44 @@ public class TestGame extends Game
 
 		addObject(planeObject);
 		addObject(directionalLightObject);
-		addObject(pointLightObject);
-		addObject(spotLightObject);
+		// addObject(pointLightObject);
+		// addObject(spotLightObject);
 
 		// getRootObject().addChild(new GameObject().addComponent(new
 		// Camera((float)Math.toRadians(70.0f),
 		// (float)Window.getWidth()/(float)Window.getHeight(), 0.01f,
 		// 1000.0f)));
 
-		GameObject testMesh1 = new GameObject().addComponent(new MeshRenderer(mesh2, material));
-		GameObject testMesh2 = new GameObject().addComponent(new MeshRenderer(mesh2, material));
-		GameObject testMesh3 = new GameObject().addComponent(new TrackMainCameraComponent()).addComponent(new MeshRenderer(tempMesh, material));
+		// GameObject testMesh1 = new GameObject().addComponent(new
+		// MeshRenderer(mesh2, material));
+		// GameObject testMesh2 = new GameObject().addComponent(new
+		// MeshRenderer(mesh2, material));
+		GameObject watchingMonkey = new GameObject().addComponent(new TrackMainCameraComponent()).addComponent(new MeshRenderer(tempMesh, material));
 
-		testMesh1.getTransform().getPos().set(0, 2, 0);
-		testMesh1.getTransform().setRot(new Quaternion(new Vector3f(0, 1, 0), 0.4f));
+		// testMesh1.getTransform().getPos().set(0, 2, 0);
+		// testMesh1.getTransform().setRot(new Quaternion(new Vector3f(0, 1, 0),
+		// 0.4f));
 
-		testMesh2.getTransform().getPos().set(0, 0, 5);
+		// testMesh2.getTransform().getPos().set(0, 0, 5);
 
-		testMesh1.addChild(testMesh2);
-		testMesh2.addChild(
-		// addObject(
-		new GameObject().addComponent(new FreeLook(0.5f)).addComponent(new FreeMove(10.0f)).addComponent(new Camera((float) Math.toRadians(70.0f), (float) Window.getWidth() / (float) Window.getHeight(), 0.01f, 1000.0f)));
+		// testMesh1.addChild(testMesh2);
 
-		addObject(testMesh1);
-		addObject(testMesh3);
+		GameObject camera = new GameObject().addComponent(new FreeLook(0.5f)).addComponent(new FreeMove(10.0f)).addComponent(new Camera((float) Math.toRadians(70.0f), (float) Window.getWidth() / (float) Window.getHeight(), 0.01f, 1000.0f));
 
-		testMesh3.getTransform().getPos().set(5, 5, 5);
-		testMesh3.getTransform().setRot(new Quaternion(new Vector3f(0, 1, 0), (float) Math.toRadians(-70.0f)));
+		// addObject(testMesh1);
+		addObject(watchingMonkey);
 
-		addObject(new GameObject().addComponent(new MeshRenderer(new Mesh("monkey3.obj"), material2)));
+		watchingMonkey.getTransform().getPos().set(5, 5, 5);
+		watchingMonkey.getTransform().setRot(new Quaternion(new Vector3f(0, 1, 0), (float) Math.toRadians(-70.0f)));
+
+		// addObject(new GameObject().addComponent(new MeshRenderer(new
+		// Mesh("monkey3.obj"), material2)));
 
 		directionalLight.getTransform().setRot(new Quaternion(new Vector3f(1, 0, 0), (float) Math.toRadians(-45)));
+		addObject(camera);
+
+		camera.getTransform().getPos().set(watchingMonkey.getTransform().getPos());
+		camera.getTransform().getPos().setX(camera.getTransform().getPos().getX() + 5);
+		camera.getTransform().getRot().set(watchingMonkey.getTransform().getRot());
 	}
 }
