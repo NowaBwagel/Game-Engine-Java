3ccc62c963e52d3a2ebb59ce739aa35db7c3c1dd
missmag <noah.bergl@gmail.com>
2014-04-15 07:04:45 -0500

    Maze stuff

--- src/com/base/mazeGame/Maze.java
+++ src/com/base/mazeGame/Maze.java
@@ -1,7 +1,11 @@
 package com.base.mazeGame;
 
 import com.base.engine.core.Game;
+import com.base.engine.rendering.Vertex;
 
-public class Maze extends Game{
+public class Maze extends Game {
+    public void init() {
+	Vertex[] cellVertex = new Vertex[] {};
+    }
 
 }
